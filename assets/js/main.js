9729140a08c241dcdf90e5b932b8e7271b906eb3
Roopesh Krishnan <rk47c4@gmail.com>
2022-03-03 18:00:38 +0530

    gallery(work section) added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,19 @@ navLinks.forEach((l) => {
 })
 
 
-$(document).ready(function(){
-    $("p").click(function(){
-      $(this).hide();
-    });
-  }); 
+// Back to top button
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back_to_top').fadeIn('slow');
+    } else {
+      $('.back_to_top').fadeOut('slow');
+    }
+  });
+
+  $('.back_to_top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
